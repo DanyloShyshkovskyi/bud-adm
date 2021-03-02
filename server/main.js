4c9d5c1abe383a407e5086dc25d3c8cc8a86c98e
DanyloShyshkovskyi <danik808@gmail.com>
2021-03-02 16:48:19 +0200

    server add

---
+++ server/main.js
@@ -0,0 +1,8 @@
+const { configure } = require('@testing-library/react')
+const config = require('config')
+const app = require('./src/app')
+
+
+const port = process.env.PORT || config.get('port')
+
+app.listen(port, ()=> console.log('server listen at '+ port))
